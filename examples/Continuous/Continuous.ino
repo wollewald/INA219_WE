981ccfbfa78ffb080caa1286d646e0201e1bb3de
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2020-08-08 17:28:46 +0200

    Add files via upload

--- examples/Continuous/Continuous.ino
+++ examples/Continuous/Continuous.ino
@@ -1,129 +1,100 @@
 /***************************************************************************
-* Example sketch for the ADS1115_WE library
+* Example sketch for the INA219_WE library
 *
-* This sketch shows how to use the ADS1115 in continuous mode. 
+* This sketch shows how to use the INA219 module in continuous mode. 
 *  
 * Further information can be found on:
-* https://wolles-elektronikkiste.de/ads1115
+* http://wolles-elektronikkiste.de/ina219
 * 
 ***************************************************************************/
+#include <Wire.h>
+#include <INA219_WE.h>
+#define I2C_ADDRESS 0x40
 
-#include<ADS1115_WE.h> 
-#include<Wire.h>
-#define I2C_ADDRESS 0x48
-
-ADS1115_WE adc(I2C_ADDRESS);
-// ADS1115_WE adc = ADS1115_WE(); // Alternative: uses default address 0x48
+INA219_WE ina219(I2C_ADDRESS);
+// INA219_WE ina219 = INA219_WE(); // Alternative: sets default address 0x40
 
 void setup() {
-  Wire.begin();
   Serial.begin(9600);
-  if(!adc.init()){
-    Serial.println("ADS1115 not connected!");
-  }
+  Wire.begin();
+  ina219.init();
 
-  /* Set the voltage range of the ADC to adjust the gain
-   * Please note that you must not apply more than VDD + 0.3V to the input pins!
-   * 
-   * ADS1115_RANGE_6144  ->  +/- 6144 mV
-   * ADS1115_RANGE_4096  ->  +/- 4096 mV
-   * ADS1115_RANGE_2048  ->  +/- 2048 mV (default)
-   * ADS1115_RANGE_1024  ->  +/- 1024 mV
-   * ADS1115_RANGE_0512  ->  +/- 512 mV
-   * ADS1115_RANGE_0256  ->  +/- 256 mV
-   */
-  adc.setVoltageRange_mV(ADS1115_RANGE_6144); //comment line/change parameter to change range
-
-  /* Set the inputs to be compared
-   *  
-   *  ADS1115_COMP_0_1    ->  compares 0 with 1 (default)
-   *  ADS1115_COMP_0_3    ->  compares 0 with 3
-   *  ADS1115_COMP_1_3    ->  compares 1 with 3
-   *  ADS1115_COMP_2_3    ->  compares 2 with 3
-   *  ADS1115_COMP_0_GND  ->  compares 0 with GND
-   *  ADS1115_COMP_1_GND  ->  compares 1 with GND
-   *  ADS1115_COMP_2_GND  ->  compares 2 with GND
-   *  ADS1115_COMP_3_GND  ->  compares 3 with GND
-   */
-  adc.setCompareChannels(ADS1115_COMP_0_GND); //comment line/change parameter to change channel
-
-  /* Set number of conversions after which the alert pin will be active
-   * - or you can disable the alert 
-   *  
-   *  ADS1115_ASSERT_AFTER_1  -> after 1 conversion
-   *  ADS1115_ASSERT_AFTER_2  -> after 2 conversions
-   *  ADS1115_ASSERT_AFTER_4  -> after 4 conversions
-   *  ADS1115_DISABLE_ALERT   -> disable comparator / alert pin (default) 
-   */
-  //adc.setAlertPinMode(ADS1115_ASSERT_AFTER_1); //uncomment if you want to change the default
-
-  /* Set the conversion rate in SPS (samples per second)
-   * Options should be self-explaining: 
-   * 
-   *  ADS1115_8_SPS 
-   *  ADS1115_16_SPS  
-   *  ADS1115_32_SPS 
-   *  ADS1115_64_SPS  
-   *  ADS1115_128_SPS (default)
-   *  ADS1115_250_SPS 
-   *  ADS1115_475_SPS 
-   *  ADS1115_860_SPS 
-   */
-  // adc.setConvRate(ADS1115_8_SPS); //uncomment if you want to change the default
-
-  /* Set continuous or single shot mode:
-   * 
-   *  ADS1115_CONTINUOUS  ->  continuous mode
-   *  ADS1115_SINGLE     ->  single shot mode (default)
-   */
-  adc.setMeasureMode(ADS1115_CONTINUOUS); //comment line/change parameter to change mode
-
-   /* Choose maximum limit or maximum and minimum alert limit (window)in Volt - alert pin will 
-   *  be active when measured values are beyond the maximum limit or outside the window 
-   *  Upper limit first: setAlertLimit_V(MODE, maximum, minimum)
-   *  In max limit mode the minimum value is the limit where the alert pin will be deactivated (if 
-   *  not latched)  
-   * 
-   *  ADS1115_MAX_LIMIT
-   *  ADS1115_WINDOW
-   * 
-   */
-  //adc.setAlertModeAndLimit_V(ADS1115_MAX_LIMIT, 3.0, 1.5); //uncomment if you want to change the default
+  /* Set ADC Mode for Bus and ShuntVoltage
+  * Mode *            * Res / Samples *       * Conversion Time *
+  BIT_MODE_9        9 Bit Resolution             84 µs
+  BIT_MODE_10       10 Bit Resolution            148 µs  
+  BIT_MODE_11       11 Bit Resolution            276 µs
+  BIT_MODE_12       12 Bit Resolution            532 µs  (DEFAULT)
+  SAMPLE_MODE_2     Mean Value 2 samples         1,06 ms
+  SAMPLE_MODE_4     Mean Value 4 samples         2,13 ms
+  SAMPLE_MODE_8     Mean Value 8 samples         4,26 ms
+  SAMPLE_MODE_16    Mean Value 16 samples        8,51 ms     
+  SAMPLE_MODE_32    Mean Value 32 samples        17,02 ms
+  SAMPLE_MODE_64    Mean Value 64 samples        34,05 ms
+  SAMPLE_MODE_128   Mean Value 128 samples       68,10 ms
+  */
+  //ina219.setADCMode(SAMPLE_MODE_128); // choose mode and uncomment for change of default
   
-  /* Enable or disable latch. If latch is enabled the alarm pin will be active until the
-   * conversion register is read (getResult functions). If disabled the alarm pin will be
-   * deactivated with next value within limits. 
-   *  
-   *  ADS1115_LATCH_DISABLED (default)
-   *  ADS1115_LATCH_ENABLED
-   */
-  //adc.setAlertLatch(ADS1115_LATCH_ENABLED); //uncomment if you want to change the default
+  /* Set measure mode
+  POWER_DOWN - INA219 switched off
+  TRIGGERED  - measurement on demand
+  ADC_OFF    - Analog/Digital Converter switched off
+  CONTINUOUS  - Continuous measurements (DEFAULT)
+  */
+  // ina219.setMeasureMode(CONTINUOUS); // choose mode and uncomment for change of default
+  
+  /* Set PGain
+  * Gain *  * Shunt Voltage Range *   * Max Current *
+   PG_40       40 mV                    0,4 A
+   PG_80       80 mV                    0,8 A
+   PG_160      160 mV                   1,6 A
+   PG_320      320 mV                   3,2 A (DEFAULT)
+  */
+  // ina219.setPGain(PG_320); // choose gain and uncomment for change of default
+  
+  /* Set Bus Voltage Range
+   BRNG_16   -> 16 V
+   BRNG_32   -> 32 V (DEFAULT)
+  */
+  // ina219.setBusRange(BRNG_32); // choose range and uncomment for change of default
 
-  /* Sets the alert pin polarity if active:
-   *  
-   * Enable or disable latch. If latch is enabled the alarm pin will be active until the
-   * conversion register is read (getResult functions). If disabled the alarm pin will be
-   * deactivated with next value within limits. 
-   *  
-   * ADS1115_ACT_LOW  ->  active low (default)   
-   * ADS1115_ACT_HIGH ->  active high
-   */
-  //adc.setAlertPol(ADS1115_ACT_LOW); //uncomment if you want to change the default
- 
-  /* With this function the alert pin will be active, when a conversion is ready.
-   * In order to deactivate, use the setAlertLimit_V function  
-   */
-  //adc.setAlertPinToConversionReady(); //uncomment if you want to change the default
+  Serial.println("INA219 Current Sensor Example Sketch - Continous");
 
-  Serial.println("ADS1115 Example Sketch - Continuous Mode");
-  Serial.println();
+  /* If the current values delivered by the INA219 differ by a constant factor
+     from values obtained with calibrated equipment you can define a correction factor.
+     Correction factor = current delivered from calibrated equipment / current delivered by INA219
+  */
+  // ina219.setCorrectionFactor(0.98); // insert your correction factor if necessary
+  
 }
 
 void loop() {
-  float voltage = 0.0;
-  voltage = adc.getResult_V(); // alternative: getResult_mV for Millivolt
-  Serial.print("Channel 0 vs GND [V]: ");
-  Serial.println(voltage);
-  Serial.println("-------------------------------");
-  delay(2000);
+  float shuntVoltage_mV = 0.0;
+  float loadVoltage_V = 0.0;
+  float busVoltage_V = 0.0;
+  float current_mA = 0.0;
+  float power_mW = 0.0; 
+  bool ina219_overflow = false;
+  
+  shuntVoltage_mV = ina219.getShuntVoltage_mV();
+  busVoltage_V = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getBusPower();
+  loadVoltage_V  = busVoltage_V + (shuntVoltage_mV/1000);
+  ina219_overflow = ina219.getOverflow();
+  
+  Serial.print("Shunt Voltage [mV]: "); Serial.println(shuntVoltage_mV);
+  Serial.print("Bus Voltage [V]: "); Serial.println(busVoltage_V);
+  Serial.print("Load Voltage [V]: "); Serial.println(loadVoltage_V);
+  Serial.print("Current[mA]: "); Serial.println(current_mA);
+  Serial.print("Bus Power [mW]: "); Serial.println(power_mW);
+  if(!ina219_overflow){
+    Serial.println("Values OK - no overflow");
+  }
+  else{
+    Serial.println("Overflow! Choose higher PGAIN");
+  }
+  Serial.println();
+  
+  delay(3000);
 }
