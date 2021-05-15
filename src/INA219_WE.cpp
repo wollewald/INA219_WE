28553b7195498a71261adcac2890ab454edf3424
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2021-05-15 11:48:09 +0200

    Add files via upload

--- src/INA219_WE.cpp
+++ src/INA219_WE.cpp
@@ -17,184 +17,196 @@
 #include "INA219_WE.h"
 
 INA219_WE::INA219_WE(int addr){
-	i2cAddress = addr;
+    _wire = &Wire;
+    i2cAddress = addr;   
 }
 
 INA219_WE::INA219_WE(){
-	i2cAddress = 0x40;
+    _wire = &Wire;
+    i2cAddress = 0x40;   
 }
-	
-bool INA219_WE::init(){	
-	if( !reset_INA219() )
+
+INA219_WE::INA219_WE(TwoWire *w, int addr){
+    _wire = w;
+    i2cAddress = addr; 
+}
+
+INA219_WE::INA219_WE(TwoWire *w){
+    _wire = w;
+    i2cAddress = 0x40;
+}
+    
+bool INA219_WE::init(){ 
+    if( !reset_INA219() )
     {
         return false;
     }
-	setADCMode(BIT_MODE_12);
-	setMeasureMode(CONTINUOUS);
-	setPGain(PG_320);
-	setBusRange(BRNG_32);
-	calc_overflow = false;
+    setADCMode(BIT_MODE_12);
+    setMeasureMode(CONTINUOUS);
+    setPGain(PG_320);
+    setBusRange(BRNG_32);
+    calc_overflow = false;
     
     return true;
 }
 
 bool INA219_WE::reset_INA219(){
-	byte ack = writeRegister(INA219_CONF_REG, INA219_RST); 
+    byte ack = writeRegister(INA219_CONF_REG, INA219_RST); 
     return ack == 0;
 }
 
 void INA219_WE::setCorrectionFactor(float corr){
-	calValCorrected = calVal * corr;
-	writeRegister(INA219_CAL_REG, calValCorrected);
+    calValCorrected = calVal * corr;
+    writeRegister(INA219_CAL_REG, calValCorrected);
 }
 
 void INA219_WE::setADCMode(INA219_ADC_MODE mode){
-	deviceADCMode = mode;
-	uint16_t currentConfReg = readRegister(INA219_CONF_REG);
-	currentConfReg &= ~(0x0780);  
-	currentConfReg &= ~(0x0078);
-	uint16_t adcMask = mode<<3;
-	currentConfReg |= adcMask;
-	adcMask = mode<<7;
-	currentConfReg |= adcMask;
-	writeRegister(INA219_CONF_REG, currentConfReg);
+    deviceADCMode = mode;
+    uint16_t currentConfReg = readRegister(INA219_CONF_REG);
+    currentConfReg &= ~(0x0780);  
+    currentConfReg &= ~(0x0078);
+    uint16_t adcMask = mode<<3;
+    currentConfReg |= adcMask;
+    adcMask = mode<<7;
+    currentConfReg |= adcMask;
+    writeRegister(INA219_CONF_REG, currentConfReg);
 }
 
 void INA219_WE::setMeasureMode(INA219_MEASURE_MODE mode){
-	deviceMeasureMode = mode;
-	uint16_t currentConfReg = readRegister(INA219_CONF_REG);
-	currentConfReg &= ~(0x0007);
-	currentConfReg |= deviceMeasureMode;
-	writeRegister(INA219_CONF_REG, currentConfReg);
+    deviceMeasureMode = mode;
+    uint16_t currentConfReg = readRegister(INA219_CONF_REG);
+    currentConfReg &= ~(0x0007);
+    currentConfReg |= deviceMeasureMode;
+    writeRegister(INA219_CONF_REG, currentConfReg);
 }
 
 void INA219_WE::setPGain(INA219_PGAIN gain){
-	devicePGain = gain;
-	uint16_t currentConfReg = readRegister(INA219_CONF_REG);
-	currentConfReg &= ~(0x1800);
-	currentConfReg |= devicePGain;
-	writeRegister(INA219_CONF_REG, currentConfReg);
-	
-	switch(devicePGain){
-		case PG_40:
-			calVal = 20480;
-			currentDivider_mA = 50.0;
-			pwrMultiplier_mW = 0.4;
-			break;
-		case PG_80:
-			calVal = 10240;
-			currentDivider_mA = 25.0;
-			pwrMultiplier_mW = 0.8;
-			break;
-		case PG_160:
-			calVal = 8192;
-			currentDivider_mA = 20.0;
-			pwrMultiplier_mW = 1.0;
-			break;
-		case PG_320:
-			calVal = 4096;
-			currentDivider_mA = 10.0;
-			pwrMultiplier_mW = 2.0;
-			break;
-	}
-	
-	writeRegister(INA219_CAL_REG, calVal);
-			
+    devicePGain = gain;
+    uint16_t currentConfReg = readRegister(INA219_CONF_REG);
+    currentConfReg &= ~(0x1800);
+    currentConfReg |= devicePGain;
+    writeRegister(INA219_CONF_REG, currentConfReg);
+    
+    switch(devicePGain){
+        case PG_40:
+            calVal = 20480;
+            currentDivider_mA = 50.0;
+            pwrMultiplier_mW = 0.4;
+            break;
+        case PG_80:
+            calVal = 10240;
+            currentDivider_mA = 25.0;
+            pwrMultiplier_mW = 0.8;
+            break;
+        case PG_160:
+            calVal = 8192;
+            currentDivider_mA = 20.0;
+            pwrMultiplier_mW = 1.0;
+            break;
+        case PG_320:
+            calVal = 4096;
+            currentDivider_mA = 10.0;
+            pwrMultiplier_mW = 2.0;
+            break;
+    }
+    
+    writeRegister(INA219_CAL_REG, calVal);
+            
 }
 
 void INA219_WE::setBusRange(INA219_BUS_RANGE range){
-	deviceBusRange = range;
-	uint16_t currentConfReg = readRegister(INA219_CONF_REG);
-	currentConfReg &= ~(0x2000);
-	currentConfReg |= deviceBusRange;
-	writeRegister(INA219_CONF_REG, currentConfReg);
+    deviceBusRange = range;
+    uint16_t currentConfReg = readRegister(INA219_CONF_REG);
+    currentConfReg &= ~(0x2000);
+    currentConfReg |= deviceBusRange;
+    writeRegister(INA219_CONF_REG, currentConfReg);
 }
 
 float INA219_WE::getShuntVoltage_mV(){
-	int16_t val;
-	val = (int16_t) readRegister(INA219_SHUNT_REG);
-	return (val * 0.01);	
+    int16_t val;
+    val = (int16_t) readRegister(INA219_SHUNT_REG);
+    return (val * 0.01);    
 }
 
 
 float INA219_WE::getBusVoltage_V(){
-	uint16_t val;
-	val = readRegister(INA219_BUS_REG);
-	val = ((val>>3) * 4);
-	return (val * 0.001);
+    uint16_t val;
+    val = readRegister(INA219_BUS_REG);
+    val = ((val>>3) * 4);
+    return (val * 0.001);
 }
 
 
 float INA219_WE::getCurrent_mA(){
-	int16_t val;
-	val = (int16_t)readRegister(INA219_CURRENT_REG);
-	return (val / currentDivider_mA);
+    int16_t val;
+    val = (int16_t)readRegister(INA219_CURRENT_REG);
+    return (val / currentDivider_mA);
 }
 
 
 float INA219_WE::getBusPower(){
-	uint16_t val;
-	val = readRegister(INA219_PWR_REG);
-	return (val * pwrMultiplier_mW);
+    uint16_t val;
+    val = readRegister(INA219_PWR_REG);
+    return (val * pwrMultiplier_mW);
 }
 
 bool INA219_WE::getOverflow(){
-	uint16_t val;
-	val = readRegister(INA219_BUS_REG);
-	bool ovf = (val & 1);
-	return ovf;
+    uint16_t val;
+    val = readRegister(INA219_BUS_REG);
+    bool ovf = (val & 1);
+    return ovf;
 }
 
 
 void INA219_WE::startSingleMeasurement(){
-	uint16_t val = readRegister(INA219_BUS_REG); // clears CNVR (Conversion Ready) Flag
-	val = readRegister(INA219_CONF_REG);
-	writeRegister(INA219_CONF_REG, val);
-	uint16_t convReady = 0x0000;
-	while(!convReady){
-		convReady = ((readRegister(INA219_BUS_REG)) & 0x0002); // checks if sampling is completed
-	}
+    uint16_t val = readRegister(INA219_BUS_REG); // clears CNVR (Conversion Ready) Flag
+    val = readRegister(INA219_CONF_REG);
+    writeRegister(INA219_CONF_REG, val);
+    uint16_t convReady = 0x0000;
+    while(!convReady){
+        convReady = ((readRegister(INA219_BUS_REG)) & 0x0002); // checks if sampling is completed
+    }
 }
 
 void INA219_WE::powerDown(){
-	confRegCopy = readRegister(INA219_CONF_REG);
-	setMeasureMode(POWER_DOWN);
+    confRegCopy = readRegister(INA219_CONF_REG);
+    setMeasureMode(POWER_DOWN);
 }
 
 void INA219_WE::powerUp(){
-	writeRegister(INA219_CONF_REG, confRegCopy);
-    delayMicroseconds(40);	
+    writeRegister(INA219_CONF_REG, confRegCopy);
+    delayMicroseconds(40);  
 }
-	
+    
 
 /************************************************ 
-	private functions
+    private functions
 *************************************************/
 
 byte INA219_WE::writeRegister(uint8_t reg, uint16_t val){
-  Wire.beginTransmission(i2cAddress);
+  _wire->beginTransmission(i2cAddress);
   uint8_t lVal = val & 255;
   uint8_t hVal = val >> 8;
-  Wire.write(reg);
-  Wire.write(hVal);
-  Wire.write(lVal);
-  return Wire.endTransmission();
+  _wire->write(reg);
+  _wire->write(hVal);
+  _wire->write(lVal);
+  return _wire->endTransmission();
 }
   
 uint16_t INA219_WE::readRegister(uint8_t reg){
   uint8_t MSByte = 0, LSByte = 0;
   uint16_t regValue = 0;
-  Wire.beginTransmission(i2cAddress);
-  Wire.write(reg);
-  Wire.endTransmission();
-  Wire.requestFrom(i2cAddress,2);
-  if(Wire.available()){
-    MSByte = Wire.read();
-    LSByte = Wire.read();
+  _wire->beginTransmission(i2cAddress);
+  _wire->write(reg);
+  _wire->endTransmission();
+  _wire->requestFrom(i2cAddress,2);
+  if(_wire->available()){
+    MSByte = _wire->read();
+    LSByte = _wire->read();
   }
   regValue = (MSByte<<8) + LSByte;
   return regValue;
 }
-	
+    
 
 
--- src/INA219_WE.h
+++ src/INA219_WE.h
@@ -28,88 +28,92 @@
 #include <Wire.h>
 
 /* registers */
-#define INA219_ADDRESS  	0x40
-#define INA219_CONF_REG 	0x00 //Configuration Register
+#define INA219_ADDRESS      0x40
+#define INA219_CONF_REG     0x00 //Configuration Register
 #define INA219_SHUNT_REG    0x01 //Shunt Voltage Register
-#define INA219_BUS_REG    	0x02 //Bus Voltage Register
-#define INA219_PWR_REG     	0x03 //Power Register 
-#define INA219_CURRENT_REG 	0x04 //Current flowing through Shunt
-#define INA219_CAL_REG     	0x05 //Calibration Register 
+#define INA219_BUS_REG      0x02 //Bus Voltage Register
+#define INA219_PWR_REG      0x03 //Power Register 
+#define INA219_CURRENT_REG  0x04 //Current flowing through Shunt
+#define INA219_CAL_REG      0x05 //Calibration Register 
 
 /* parameters */
 #define INA219_RST             0x8000
 
 typedef enum INA219_ADC_MODE{
-	BIT_MODE_9 		= 0b00000000,   
-	BIT_MODE_10 	= 0b00000001,
-	BIT_MODE_11 	= 0b00000010,
-	BIT_MODE_12 	= 0b00000011,
-	SAMPLE_MODE_2 	= 0b00001001,
-	SAMPLE_MODE_4 	= 0b00001010,
-	SAMPLE_MODE_8	= 0b00001011,
-	SAMPLE_MODE_16 	= 0b00001100,
-	SAMPLE_MODE_32 	= 0b00001101,
-	SAMPLE_MODE_64 	= 0b00001110,
-	SAMPLE_MODE_128 = 0b00001111
+    BIT_MODE_9      = 0b00000000,   
+    BIT_MODE_10     = 0b00000001,
+    BIT_MODE_11     = 0b00000010,
+    BIT_MODE_12     = 0b00000011,
+    SAMPLE_MODE_2   = 0b00001001,
+    SAMPLE_MODE_4   = 0b00001010,
+    SAMPLE_MODE_8   = 0b00001011,
+    SAMPLE_MODE_16  = 0b00001100,
+    SAMPLE_MODE_32  = 0b00001101,
+    SAMPLE_MODE_64  = 0b00001110,
+    SAMPLE_MODE_128 = 0b00001111
 } adcMode;
 
 typedef enum INA219_MEASURE_MODE{
-	POWER_DOWN 		= 0b00000000,
-	TRIGGERED		= 0b00000011,
-	ADC_OFF			= 0b00000100,
-	CONTINUOUS		= 0b00000111,
-	CONTINOUS		= 0b00000111  // keeping misspelled enum for backwards compatibility.
+    POWER_DOWN      = 0b00000000,
+    TRIGGERED       = 0b00000011,
+    ADC_OFF         = 0b00000100,
+    CONTINUOUS      = 0b00000111,
+    CONTINOUS       = 0b00000111  // keeping misspelled enum for backwards compatibility.
 } measureMode;
 
 typedef enum INA219_PGAIN{
-	PG_40 		= 0x0000,
-	PG_80		= 0x0800,
-	PG_160		= 0x1000,
-	PG_320		= 0x1800
+    PG_40       = 0x0000,
+    PG_80       = 0x0800,
+    PG_160      = 0x1000,
+    PG_320      = 0x1800
 } pGain;
 
 typedef enum INA219_BUS_RANGE{
-	BRNG_16 		= 0x0000,
-	BRNG_32 		= 0x2000,
+    BRNG_16         = 0x0000,
+    BRNG_32         = 0x2000,
 } busRange;
 
 
 class INA219_WE
 {
-public:	
-	INA219_WE(int addr);
-	INA219_WE();			//sets default I2C Address 0x40
+public: 
+    // Constructors: if not passed 0x40 / Wire will be set as address / wire object
+    INA219_WE(int addr);
+    INA219_WE();
+    INA219_WE(TwoWire *w, int addr);
+    INA219_WE(TwoWire *w);
   
-	bool init();
-	bool reset_INA219();
-	void setCorrectionFactor(float corr);
-	void setADCMode(INA219_ADC_MODE mode);
-	void setMeasureMode(INA219_MEASURE_MODE mode);
-	void setPGain(INA219_PGAIN gain);
-	void setBusRange(INA219_BUS_RANGE range);
-	float getShuntVoltage_mV();
-	float getBusVoltage_V();
-	float getCurrent_mA();
-	float getBusPower();
-	bool getOverflow();
-	void startSingleMeasurement();
-	void powerDown();
-	void powerUp(); 
-	
+    bool init();
+    bool reset_INA219();
+    void setCorrectionFactor(float corr);
+    void setADCMode(INA219_ADC_MODE mode);
+    void setMeasureMode(INA219_MEASURE_MODE mode);
+    void setPGain(INA219_PGAIN gain);
+    void setBusRange(INA219_BUS_RANGE range);
+    float getShuntVoltage_mV();
+    float getBusVoltage_V();
+    float getCurrent_mA();
+    float getBusPower();
+    bool getOverflow();
+    void startSingleMeasurement();
+    void powerDown();
+    void powerUp(); 
+    
 private:
-	INA219_ADC_MODE deviceADCMode;
-	INA219_MEASURE_MODE deviceMeasureMode;
-	INA219_PGAIN devicePGain;
-	INA219_BUS_RANGE deviceBusRange;
-	int i2cAddress;
-	uint16_t calVal;
-	uint16_t calValCorrected;
-	uint16_t confRegCopy;
-	float currentDivider_mA;
-	float pwrMultiplier_mW;
-	bool calc_overflow;
-	byte writeRegister(uint8_t reg, uint16_t val);
-	uint16_t readRegister(uint8_t reg);
+    INA219_ADC_MODE deviceADCMode;
+    INA219_MEASURE_MODE deviceMeasureMode;
+    INA219_PGAIN devicePGain;
+    INA219_BUS_RANGE deviceBusRange;
+    TwoWire *_wire;
+    int i2cAddress;
+    uint16_t calVal;
+    uint16_t calValCorrected;
+    uint16_t confRegCopy;
+    float currentDivider_mA;
+    float pwrMultiplier_mW;
+    bool calc_overflow;
+    byte writeRegister(uint8_t reg, uint16_t val);
+    uint16_t readRegister(uint8_t reg);
 
 };
 
