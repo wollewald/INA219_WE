8114f59219fb0014b9cb039a391b18e82e1bb460
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2025-09-28 16:12:02 +0200

    Add files via upload

--- examples/Triggered/Triggered.ino
+++ examples/Triggered/Triggered.ino
@@ -22,7 +22,7 @@
 INA219_WE ina219 = INA219_WE(I2C_ADDRESS);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Wire.begin();
   if(!ina219.init()){
     Serial.println("INA219 not connected!");
@@ -30,58 +30,68 @@ void setup() {
 }
 
   /* Set ADC Mode for Bus and ShuntVoltage
-  * Mode *            * Res / Samples *       * Conversion Time *
-  BIT_MODE_9        9 Bit Resolution             84 µs
-  BIT_MODE_10       10 Bit Resolution            148 µs  
-  BIT_MODE_11       11 Bit Resolution            276 µs
-  BIT_MODE_12       12 Bit Resolution            532 µs  (DEFAULT)
-  SAMPLE_MODE_2     Mean Value 2 samples         1.06 ms
-  SAMPLE_MODE_4     Mean Value 4 samples         2.13 ms
-  SAMPLE_MODE_8     Mean Value 8 samples         4.26 ms
-  SAMPLE_MODE_16    Mean Value 16 samples        8.51 ms     
-  SAMPLE_MODE_32    Mean Value 32 samples        17.02 ms
-  SAMPLE_MODE_64    Mean Value 64 samples        34.05 ms
-  SAMPLE_MODE_128   Mean Value 128 samples       68.10 ms
+    * Mode *                 * Res / Samples *        * Conversion Time *
+    INA219_BIT_MODE_9        9 Bit Resolution             84 µs
+    INA219_BIT_MODE_10       10 Bit Resolution            148 µs  
+    INA219_BIT_MODE_11       11 Bit Resolution            276 µs
+    INA219_BIT_MODE_12       12 Bit Resolution            532 µs  (DEFAULT)
+    INA219_SAMPLE_MODE_2     Mean Value 2 samples         1.06 ms
+    INA219_SAMPLE_MODE_4     Mean Value 4 samples         2.13 ms
+    INA219_SAMPLE_MODE_8     Mean Value 8 samples         4.26 ms
+    INA219_SAMPLE_MODE_16    Mean Value 16 samples        8.51 ms     
+    INA219_SAMPLE_MODE_32    Mean Value 32 samples        17.02 ms
+    INA219_SAMPLE_MODE_64    Mean Value 64 samples        34.05 ms
+    INA219_SAMPLE_MODE_128   Mean Value 128 samples       68.10 ms
+    
+    If you measure both current and bus voltage (which is the default), the conversion time doubles.
   */
-  // ina219.setADCMode(BIT_MODE_12); // choose mode and uncomment for change of default
+  //ina219.setADCMode(INA219_SAMPLE_MODE_128); // choose mode and uncomment for change of default
   
   /* Set measure mode
-  POWER_DOWN - INA219 switched off
-  TRIGGERED  - measurement on demand
-  ADC_OFF    - Analog/Digital Converter switched off
-  CONTINUOUS  - Continuous measurements (DEFAULT)
+    INA219_POWER_DOWN              - INA219 switched off
+    INA219_TRIGGERED               - measurement on demand, current and bus
+    INA219_TRIGGERED_CURRENT_ONLY  - on demand, current only
+    INA219_TRIGGERED_BUS_ONLY      - on demand, bus voltage only
+    INA219_ADC_OFF                 - analog/digital converter switched off
+    INA219_CONTINUOUS              - continuous measurements (DEFAULT)
+    INA219_CONTINUOUS_CURRENT_ONLY - continuous, current only
+    INA219_CONTINUOUS_BUS_ONLY     - continuous, bus voltage only
   */
-  ina219.setMeasureMode(TRIGGERED); // Triggered measurements for this example
+  ina219.setMeasureMode(INA219_TRIGGERED); // Triggered measurements for this example
   
   /* Set PGain
-  * Gain *  * Shunt Voltage Range *   * Max Current *
-   PG_40       40 mV                    0.4 A
-   PG_80       80 mV                    0.8 A
-   PG_160      160 mV                   1.6 A
-   PG_320      320 mV                   3.2 A (DEFAULT)
+      * Gain *      * Shunt Voltage Range *   * Max Current (if shunt is 0.1 ohms) *
+    INA219_PG_40          40 mV                   0.4 A
+    INA219_PG_80          80 mV                   0.8 A
+    INA219_PG_160         160 mV                  1.6 A
+    INA219_PG_320         320 mV                  3.2 A (DEFAULT)
   */
-  // ina219.setPGain(PG_320); // choose gain and uncomment for change of default
+  // ina219.setPGain(INA219_PG_320); // choose gain and uncomment for change of default
   
   /* Set Bus Voltage Range
-   BRNG_16   -> 16 V
-   BRNG_32   -> 32 V (DEFAULT)
+    INA219_BRNG_16   -> 16 V
+    INA219_BRNG_32   -> 32 V (DEFAULT)
   */
-  // ina219.setBusRange(BRNG_32); // choose range and uncomment for change of default
+  // ina219.setBusRange(INA219_BRNG_32); // choose range and uncomment for change of default
 
   /* If the current values delivered by the INA219 differ by a constant factor
-     from values obtained with calibrated equipment you can define a correction factor.
-     Correction factor = current delivered from calibrated equipment / current delivered by INA219
+    from values obtained with calibrated equipment you can define a correction factor.
+    Correction factor = current delivered from calibrated equipment / current delivered by INA219
   */
   // ina219.setCorrectionFactor(0.98); // insert your correction factor if necessary
 
   /* If you experience a shunt voltage offset, that means you detect a shunt voltage which is not 
-     zero, although the current should be zero, you can apply a correction. For this, uncomment the 
-     following function and apply the offset you have detected.   
+    zero, although the current should be zero, you can apply a correction. For this, uncomment the 
+    following function and apply the offset you have detected.   
   */
   // ina219.setShuntVoltOffset_mV(0.0); // insert the shunt voltage (millivolts) you detect at zero current
+
+  /* If you use a shunt different from 0.1 ohms (R100), you can change the shunt size using the
+    function below.
+  */
+  //ina219.setShuntSizeInOhms(0.01); 
   
   Serial.println("INA219 Current Sensor Example Sketch - Triggered Mode");
-
 }
 
 void loop() {
@@ -109,7 +119,7 @@ void loop() {
     Serial.println("Values OK - no overflow");
   }
   else{
-    Serial.println("Overflow! Choose higher PGAIN");
+    Serial.println("Overflow! Choose higher shunt voltage range or a smaller shunt.");
   }
   Serial.println();
   
